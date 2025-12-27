9e22198ca1dc3e20dc26303e951200bec99b2d55
unknown <mimji1380@gmail.com>
2025-12-28 00:35:32 +0330

    Update login page text

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 include 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $username = $_POST['username'];
+    $username = $_POST['useoorname'];
     $password = $_POST['password'];
 
     $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
